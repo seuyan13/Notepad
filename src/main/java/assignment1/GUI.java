98f6e79a387d1f14f58d5330bf94c3da43c78207
seuyan13 <yangsw1203@naver.com>
2022-08-13 20:22:17 +1200

    created the project

---
+++ src/main/java/assignment1/GUI.java
@@ -0,0 +1,32 @@
+package assignment1;
+import javax.swing.*;
+
+public class GUI {
+	JFrame window;
+	JTextArea textArea;
+	JScrollPane scrollPane;
+	
+	public static void main(String[] args) {
+		new GUI();
+	}
+	
+	public GUI() {
+		createWindow();
+		createTextArea();
+		window.setVisible(true);
+	}
+	
+	public void createWindow() {
+		window = new JFrame("159251_assignment 1_TextEditor");
+		window.setSize(800,600);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public void createTextArea() {
+		textArea = new JTextArea();
+		scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setBorder(BorderFactory.createEmptyBorder());
+		window.add(scrollPane);
+		//window.add(textArea);
+	}
+}
