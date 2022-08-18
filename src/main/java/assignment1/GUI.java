78a1bd84f4b2ffa32eddaed7370bb5b41328124e
seuyan13 <yangsw1203@naver.com>
2022-08-18 15:01:15 +1200

    update - menubar,items

--- src/main/java/assignment1/GUI.java
+++ src/main/java/assignment1/GUI.java
@@ -27,6 +27,5 @@ public class GUI {
 		scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		scrollPane.setBorder(BorderFactory.createEmptyBorder());
 		window.add(scrollPane);
-		//window.add(textArea);
 	}
 }
